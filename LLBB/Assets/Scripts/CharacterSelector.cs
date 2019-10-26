3130cb2d602b51c460cb51f3d27618353c714d5e
Kirijo <asyelsub@gmail.com>
2019-10-26 17:08:39 -0400

    button picking with debug

---
+++ LLBB/Assets/Scripts/CharacterSelector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CharacterSelector : MonoBehaviour
+{
+    public Button characterSelectOne,characterSelectTwo,characterSelectThree,characterSelectFour;
+
+    public void SelectCharacter(Button selectCharacter){
+        if(selectCharacter == characterSelectOne){
+            Debug.Log("Character1");
+        }
+        else if(selectCharacter == characterSelectTwo){
+            Debug.Log("Character2");
+        }
+        else if(selectCharacter == characterSelectThree){
+            Debug.Log("Character3");
+        }
+        else if(selectCharacter == characterSelectFour){
+            Debug.Log("Character4");
+        }
+    }
+
+}
