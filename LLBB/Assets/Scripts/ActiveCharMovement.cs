8b7f5e68ec31ad2eb4d2c8aeb41fa625518ff60c
Kirijo <asyelsub@gmail.com>
2019-10-26 18:13:45 -0400

    characters are shittily movable by selecting portraits

---
+++ LLBB/Assets/Scripts/ActiveCharMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveCharMovement : MonoBehaviour
+{
+    public Transform charOne,charTwo,charThree,charFour;
+    public static int charInt;
+    Transform activeTransform;
+    
+    void Start(){
+        activeTransform = charOne;
+    }
+    public void setActiveCharacter(int charInt){
+        Debug.Log("setActiveCharacter is triggered by" + charInt);
+        if (charInt == 1){
+            activeTransform = charOne;
+            Debug.Log("active transform = " + activeTransform.ToString());
+        }
+        if (charInt == 2){
+            activeTransform = charTwo;
+            Debug.Log("active transform = " + activeTransform.ToString());
+        }
+        if (charInt == 3){
+            activeTransform = charThree;
+            Debug.Log("active transform = " + activeTransform.ToString());
+        }
+        if (charInt == 4){
+            activeTransform = charFour;
+            Debug.Log("active transform = " + activeTransform.ToString());
+        }
+    }
+
+    void FixedUpdate(){
+        if(Input.GetKey(KeyCode.UpArrow)){
+            activeTransform.position += activeTransform.position + new Vector3(0f,0f,.1f);
+        }
+        if(Input.GetKey(KeyCode.RightArrow)){
+            
+        }
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            
+        }
+        if(Input.GetKey(KeyCode.DownArrow)){
+
+        }
+    }
+}
