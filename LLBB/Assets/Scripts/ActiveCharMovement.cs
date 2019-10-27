615f75b4b892e5df8cf1507cfe503a542e003168
Kirijo <asyelsub@gmail.com>
2019-10-27 17:52:30 -0400

    Attempt to get raycasting movement to work, begin setting up UI for inventories.

--- LLBB/Assets/Scripts/ActiveCharMovement.cs
+++ LLBB/Assets/Scripts/ActiveCharMovement.cs
@@ -6,7 +6,13 @@ public class ActiveCharMovement : MonoBehaviour
 {
     public Transform charOne,charTwo,charThree,charFour;
     public static int charInt;
+    //bool char1Moving;
     Transform activeTransform;
+    //public Transform oldTransform;
+    //public Transform newTransform;
+    bool isMoving = false;
+    Vector3 tempVect;
+    
     
     void Start(){
         activeTransform = charOne;
@@ -15,34 +21,42 @@ public class ActiveCharMovement : MonoBehaviour
         Debug.Log("setActiveCharacter is triggered by" + charInt);
         if (charInt == 1){
             activeTransform = charOne;
-            Debug.Log("active transform = " + activeTransform.ToString());
         }
         if (charInt == 2){
             activeTransform = charTwo;
-            Debug.Log("active transform = " + activeTransform.ToString());
         }
         if (charInt == 3){
             activeTransform = charThree;
-            Debug.Log("active transform = " + activeTransform.ToString());
         }
         if (charInt == 4){
             activeTransform = charFour;
-            Debug.Log("active transform = " + activeTransform.ToString());
         }
     }
 
-    void FixedUpdate(){
-        if(Input.GetKey(KeyCode.UpArrow)){
-            activeTransform.position += activeTransform.position + new Vector3(0f,0f,.1f);
-        }
-        if(Input.GetKey(KeyCode.RightArrow)){
-            
-        }
-        if(Input.GetKey(KeyCode.LeftArrow)){
-            
-        }
-        if(Input.GetKey(KeyCode.DownArrow)){
+    void Update(){
 
-        }
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float mouseRayDistance = 10000f;
+        RaycastHit rayHit = new RaycastHit();
+        Debug.DrawRay(mouseRay.origin, mouseRay.direction*mouseRayDistance,Color.yellow);
+        if(Physics.Raycast(mouseRay,out rayHit,mouseRayDistance)){
+        if(Input.GetKeyDown(KeyCode.Mouse0)){
+            
+                   //save old "active transform"
+                   
+                   isMoving = true;
+                   Vector3 tempVect = new Vector3(rayHit.point.x,0f,rayHit.point.z);
+                   Debug.Log("tempVect = " + tempVect.x + "," + tempVect.z);
+                   //find new "active transform" through rayHit.position    
+                   //lerp between the two over void Update               
+            }
+             
+        } 
+            
+        //Debug.Log("RaycastHit = " + rayHit.point);
+        //Debug.Log(isMoving);
+        /*if (isMoving){
+            activeTransform.Translate(new Vector3(rayHit.point.x,0f,rayHit.point.z));
+        } */      
     }
 }
