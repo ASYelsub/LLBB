dc6502b0ddad9b9161451bf754f046ce1eefb54c
Abby <asyelsub@gmail.com>
2019-12-30 22:43:35 -0500

    continued working on fighter selection UI

--- Assets/Scripts/PickChar.cs
+++ Assets/Scripts/PickChar.cs
@@ -6,90 +6,93 @@ using UnityEngine.UI;
 public class PickChar : MonoBehaviour
 {
     public Transform activeCharTransform;
-    public bool hoverOverDragable = false;
-    public bool dragableHeld = false;
     public Camera cam;
-    public Vector3 mousePos;
-    public Vector3 originalPos;
-    public Transform charButton1, charButton2, charButton3, charButton4;
-    int mouseOverNumber = 0;
-    public Text char1Text, char2Text, char3Text, char4Text;
-    public bool letGo = false;
+    public Text[] charText = new Text[4];
+    int characterNumber = 0;
+    public string[] charNames = new string[5];
+    public Text activeName;
+    bool hoverOverDragable = false;
+    bool dragableHeld = false;
+    Vector3 mousePos;
+    Vector3 originalPos;
     int heldNumber = 0;
    
-    public void Awake()
-    {
+    public void Awake(){
+        activeName.text = charNames[characterNumber];
         originalPos = activeCharTransform.position;
+
     }
-    public void Update()
-    {
-        if (dragableHeld == true)
-        {
-            activeCharTransform.position = Input.mousePosition;
+    public void ChangeActiveCharArray(int direction){ //0 = left, 1 = right
+        if(direction == 1 && characterNumber < charNames.Length - 1){
+            characterNumber++;
+            activeName.text = charNames[characterNumber];
         }
-        else if(dragableHeld == false || hoverOverDragable == false)
-        {
-            activeCharTransform.position = originalPos;
+        else if(direction == 1 && characterNumber == charNames.Length - 1){
+            characterNumber = 0;
+            activeName.text = charNames[characterNumber];
         }
+        if(direction == 0 && characterNumber > 0){
+            characterNumber --;
+            activeName.text = charNames[characterNumber];
+        }
+        else if(direction == 0 && characterNumber == 0){
+            characterNumber = 4;
+            activeName.text = charNames[characterNumber];
+        }
+    }
+    public void Update(){
+        Debug.Log(characterNumber);
+        mousePos = new Vector3(Input.mousePosition.x,Input.mousePosition.y,-1);
+        if (dragableHeld == true){
+            activeCharTransform.position = mousePos;}
+        else if(dragableHeld == false || hoverOverDragable == false){
+            activeCharTransform.position = originalPos;}
     }
 
-    public void MouseHover()
-    {
+    //dragging and assigning "character" in 1,2,3,4 slots functions
+    public void MouseHover(){
         hoverOverDragable = true;
     }
-    public void MouseNotHover()
-    {
+    public void MouseNotHover(){
         hoverOverDragable = false;
     }
-    public void OnMouseDown()
-    {
+    public void OnMouseDown(){
         dragableHeld = true;
-        letGo = false;
     }
-    public void OnMouseUp()
-    {
-        dragableHeld = false;
-       //mouseOverNumber = 0;
-       //Debug.Log("This runs");
-       letGo = true;
-        SetChar();
+    public void OnMouseUp(){
+     dragableHeld = false;
+     SetChar();
     }
-  
-    public void OverChar(int i)
-    {
-        if (dragableHeld == true)
-        {
-            Debug.Log("OverChar plus letGo");
+    public void OverChar(int i){
+        if (dragableHeld == true){//Debug.Log("OverChar plus letGo");
             if (i == 1)
-            { heldNumber = 1; }
+                { heldNumber = 1; }
             if (i == 2)
-            { heldNumber = 2; }
+                { heldNumber = 2; }
             if (i == 3)
-            { heldNumber = 3; }
+                { heldNumber = 3; }
             if (i == 4)
-            { heldNumber = 4; }
+                { heldNumber = 4; }
             if (i == 0) //on exit from char buttons set i as 0
-            { heldNumber = 0; }
+                { heldNumber = 0; }
         }
-
     }
-    void SetChar()
-    {
-        
+    void SetChar(){
         if (heldNumber == 1)
-        { char1Text.text = "hi";char2Text.text = "C2";char3Text.text = "C3"; char4Text.text = "C4"; }
+            { charText[0].text = "hi"; charText[1].text = "C2"; charText[2].text = "C3"; charText[3].text = "C4"; }
         if (heldNumber == 2)
-        { char1Text.text = "C1"; char2Text.text = "hi"; char3Text.text = "C3"; char4Text.text = "C4"; }
+            { charText[0].text = "C1"; charText[1].text = "hi"; charText[2].text = "C3"; charText[3].text = "C4"; }
         if (heldNumber == 3)
-        { char1Text.text = "C1"; char2Text.text = "C2"; char3Text.text = "hi"; char4Text.text = "C4"; }
+            { charText[0].text = "C1"; charText[1].text = "C2"; charText[2].text = "hi"; charText[3].text = "C4"; }
         if (heldNumber == 4)
-        { char1Text.text = "C1"; char2Text.text = "C2"; char3Text.text = "C3"; char4Text.text = "hi"; }
-        if (heldNumber == 0)
-        { char1Text.text = "C1"; char2Text.text = "C2"; char3Text.text = "C3"; char4Text.text = "C4"; }
+            { charText[0].text = "C1"; charText[1].text = "C2"; charText[2].text = "C3"; charText[3].text = "hi"; }
     }
 
-    public void BeginGame()
-    {
+    //setting who "character" is when being dragged
+
+    //closing menu
+    public void BeginGame(){
+        //triggered by pressing start button
         this.gameObject.SetActive(false);
     }
 }
