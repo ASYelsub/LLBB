0e408fc27200d5d52f432068ddf6f3f9dfd3fecc
Vera Limani <vl979@nyu.edu>
2020-01-27 16:30:04 -0500

    Scriptable Object class + Base Unit

---
+++ Assets/Scripts/VeraScripts/BaseClass.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Class", menuName = "Classes")]
+public class BaseClass : ScriptableObject
+{
+    public CombatStatSetting[] ClassStatSettings;
+
+    public void GenerateStats(BaseUnit u)
+    {
+        if (u.UnitStats == null || u.UnitStats.Count > 9) { return; }
+        for (int i = 0; i < ClassStatSettings.Length; i++)
+        {
+            u.UnitStats.Add(ClassStatSettings[i].CombatStatType, ClassStatSettings[i].GenerateStat());
+        }
+    }
+}
+
+[System.Serializable]
+public struct CombatStatSetting
+{
+	public StatType CombatStatType;
+	public float StatMaxValue;
+    public float StatBonus;
+
+    public CombatStatSetting(StatType stat, float bonus, float max)
+	{
+		CombatStatType = stat;
+		StatBonus = bonus;
+		StatMaxValue = max;
+	}
+
+	public float GenerateStat(){ return Mathf.Ceil(Random.Range(5f, StatMaxValue)) + StatBonus;}
+}
+
+public enum StatType
+{
+    NONE = 0,
+    HEALTH = 1,
+    STAMINA = 2,
+    WHAMITUDE = 3,
+    POPULARITY = 4,
+    SCHOOLSPIRIT = 5,
+    CREEPINESS = 6,
+    SKINTHICKNESS = 7,
+    INDEPENDENCE = 8,
+    EMPATHY = 9
+}
---
+++ Assets/Scripts/VeraScripts/BaseUnit.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEditor;
+
+public class BaseUnit : MonoBehaviour
+{
+	public BaseClass UnitType;
+	public Dictionary<StatType, float> UnitStats;
+
+    void Start()
+    {
+        //GenerateUnitStats();
+    }
+
+    private void Update()
+    {
+       
+    }
+
+    public void DebugStats(){
+        Debug.Log(this.name);
+		for(int i = 0;i< UnitStats.Count; i++){
+			Debug.Log(UnitStats.ElementAt(i).Key + ": " + UnitStats.ElementAt(i).Value);
+		}
+	}
+
+    public void GenerateUnitStats()
+    {
+        UnitStats = new Dictionary<StatType, float>();
+        if (UnitType != null && UnitType.ClassStatSettings.Length > 0)
+        {
+            UnitType.GenerateStats(this);
+        }
+    }
+}
+[CustomEditor(typeof(BaseUnit))]
+public class UnitEditor: Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        BaseUnit b = (BaseUnit)target;
+        if (GUILayout.Button("Generate Unit Stats"))
+        {
+            b.GenerateUnitStats();
+            b.DebugStats();
+        }
+    }
+}
