50b98735e08a3a2b8f5865577d668684949f94fa
Abigail <asyelsub@gmail.com>
2020-02-03 17:05:41 -0500

    Meeting w Vera on 2/3/20

--- Assets/Scripts/CharacterSelector.cs
+++ Assets/Scripts/CharacterSelector.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 //USAGE: this lets you click on character bubbles during combat to change who you are playing as, works with buttons
 public class CharacterSelector : MonoBehaviour
 {
-    public MoveManager moveManager;
+    //public MoveManager moveManager;
     public ActiveCharMovement activeCharMovement;
     //Intent: select active character GameObject
     public Button characterSelectOne,characterSelectTwo,characterSelectThree,characterSelectFour;
@@ -22,23 +22,30 @@ public class CharacterSelector : MonoBehaviour
         if(selectCharacter == characterSelectOne){
  //           Debug.Log("Character1");
             activeCharMovement.setActiveCharacter(1);
-            moveManager.DisplayActiveMoveset(1);
+            //moveManager.DisplayActiveMoveset(1);
         }
         else if(selectCharacter == characterSelectTwo){
  //           Debug.Log("Character2");
             activeCharMovement.setActiveCharacter(2);
-            moveManager.DisplayActiveMoveset(2);
+            //moveManager.DisplayActiveMoveset(2);
         }
         else if(selectCharacter == characterSelectThree){
 //            Debug.Log("Character3");
             activeCharMovement.setActiveCharacter(3);
-            moveManager.DisplayActiveMoveset(3);
+            //moveManager.DisplayActiveMoveset(3);
         }
         else if(selectCharacter == characterSelectFour){
 //            Debug.Log("Character4");
             activeCharMovement.setActiveCharacter(4);
-            moveManager.DisplayActiveMoveset(4);
+            //moveManager.DisplayActiveMoveset(4);
         }
+    }
+    //Create a custom button that stores the reference to the base unit on assigning 
+    [System.Serializable]
+    public struct CharacterSelectButton
+    {
+        public Button charButton;
+        public BaseUnit thisUnit;
     }
 
 }
---
+++ Assets/Scripts/MoveButtonManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class MoveButtonManager : MonoBehaviour
+{
+    public List<MoveButton> MoveButtons;
+
+    [Serializable]
+    public struct MoveButton
+	{
+        public Button uiButton;
+        public CombatMoveType moveTypeToDisplay;
+        public MoveButton(Button b, CombatMoveType c)
+		{
+            uiButton = b;
+            moveTypeToDisplay = c;
+		}
+
+        public MoveButton(Button b)
+		{
+            uiButton = b;
+            moveTypeToDisplay = CombatMoveType.EXAMPLE;
+		}
+
+        public void UpdateCombatType(CombatMoveType c)
+		{
+            moveTypeToDisplay = c;
+		}
+	}
+
+    private void Start()
+    {
+        List<Button> buttons = new List<Button>(GetComponentsInChildren<Button>());
+        MoveButtons = CreateMoveButtonList(buttons);
+    }
+
+    private List<MoveButton> CreateMoveButtonList(List<Button> buttons)
+    {
+        List<MoveButton> newMoveButtons = new List<MoveButton>();
+        foreach (Button b in buttons)
+        {
+            b.GetComponentInChildren<Text>().text = "Move: " + (newMoveButtons.Count + 1).ToString();
+            newMoveButtons.Add(new MoveButton(b));
+        }
+        return newMoveButtons;
+    }
+
+
+}
